02c0e9e831c5d66f0737dddf99bf68a3507278f5
alee <aleblasdc09@gmail.com>
2024-10-29 21:49:00 -0500

    first commit

---
+++ semana10/Controllers/ProductoController.cs
@@ -0,0 +1,58 @@
+﻿using semana10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace semana10.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+            public ActionResult Index()
+            {
+
+            List<Producto> productos = new List<Producto>();
+
+            if (Session["productos"] == null)
+
+                {
+                    //Ir base de datos               
+                    productos.Add(new Producto { Nombre = "Fresa", Descripcion = "en buen estado", Precio = 05.50m, ProductoID = 1 });
+
+                    Session["productos"] = productos;
+                }
+            else
+                {
+                    productos = (List<Producto>)Session["productos"];
+                }
+
+            return View(productos);
+            }
+
+            public ActionResult Create()
+            {
+                return View();
+            }
+
+        [HttpPost]
+        public ActionResult Create(Producto producto)
+        {
+
+            ((List<Persona>)Session["producto"]).Add(producto);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit()
+            {
+                return View();
+            }
+
+            public ActionResult Delete()
+            {
+                return View();
+            }
+    }
+}
---
+++ semana10/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace semana10.Models
+{
+    public class Persona
+    {
+        public int PersonaID { get; set; }
+        public string Nombres {get; set; }
+        public string Apellidos { get; set; }
+    }
+}
